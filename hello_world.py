100d6fa7269ac57b62ca2a81a94b5e6f58892b61
Chad Ruble <cruble4@gmail.com>
2018-11-08 17:38:12 -0500

    tests passing

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,31 @@
 # import flask here
+from flask import Flask
 
 
 # create new Flask App here
-
+app = Flask(__name__)
 
 # define routes for your new flask app
 
 
 # tell your flask app to run with debug mode on
+@app.route('/')
+def hello_world():
+    return 'Hello, world!'
+
+
+@app.route('/home')
+def home():
+    return "Welcome to an amazing Flask App!"
+
+@app.route('/myprofile')
+def myprofile():
+    return "This is my profile! It's not finished yet... :/"
+
+@app.route('/exit')
+def exit():
+    return "Thanks for looking around. Come back again soon!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
